7adf89fe2d47135cfaa81ea19a2878633ad301dc
Abubakar Bibulatov <individ101@mail.ru>
2025-04-12 00:44:03 +0300

    change searchBar

--- TodoListWork/TodoListWork/View/CustomSearchBar.swift
+++ TodoListWork/TodoListWork/View/CustomSearchBar.swift
@@ -22,6 +22,28 @@ class CustomSearchBar: UISearchBar {
         placeholder = "Find todo"
         translatesAutoresizingMaskIntoConstraints = false
         searchBarStyle = .minimal
+        barTintColor = .clear
+        
+        if let textField = value(forKey: "searchTextField") as? UITextField {
+            textField.backgroundColor = UIColor(named: "textFieldBackground")
+            textField.textColor = .white
+            textField.tintColor = .systemBlue
+            textField.attributedPlaceholder = NSAttributedString(
+                string: "Find todo",
+                attributes: [.foregroundColor: UIColor(named: "texFPlaceHolder") ?? .gray]
+            )
+            
+            if let clearButton = textField.value(forKey: "_clearButton") as? UIButton {
+                let clearImage = clearButton.image(for: .normal)?.withRenderingMode(.alwaysTemplate)
+                clearButton.setImage(clearImage, for: .normal)
+                clearButton.tintColor = UIColor(named: "texFPlaceHolder") ?? .systemGray2
+            }
+            
+            let searchImage = UIImage(systemName: "magnifyingglass")?.withRenderingMode(.alwaysTemplate)
+            setImage(searchImage, for: .search, state: .normal)
+            textField.leftView?.tintColor = UIColor(named: "texFPlaceHolder") ?? .systemGray2
+        }
     }
     
 }
+
